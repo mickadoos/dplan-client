286dcfe412cac5541861d5c419b83975c8d6df0f
Yabel <yabelrr@gmail.com>
2022-11-07 16:11:53 +0100

    Updating Plan Page and Navbar upadted dev mode

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,20 +10,30 @@ function Navbar() {
 
   return (
     <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
 
       {isLoggedIn && (
         <>
-          <button onClick={logOutUser}>Logout</button>
+          
 
+          <Link to="/plans">
+            <button>Plans</button>
+          </Link>
           <Link to="/plans/newPlan">
             <button>New Plan</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
           </Link>
-
+          <br></br>
+          <Link to={`/${user.username}/profile`}>
+            <button>User Profile</button>
+          </Link>
+          <Link to={`/${user.username}/addFriends`}>
+            <button>Add Friends</button>
+          </Link>
+          <Link to={`/${user.username}/invite`}>
+            <button>Invite</button>
+          </Link>
           <span>{user && user.name}</span>
+          <br></br>
+          <button onClick={logOutUser}>Logout</button>
         </>
       )}
 
--- src/pages/PlanPage/PlanPage.jsx
+++ src/pages/PlanPage/PlanPage.jsx
@@ -6,10 +6,38 @@ import planService from "../../services/plan.service";
 function PlanPage() {
 
   const {planId} = useParams();
+  const [plan, setPlan] = useState({});
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    planService.getPlan(planId)
+    .then(response => {
+        setPlan(response.data);
+    })
+
+}, [planId])
+
   return (
     <div>
       <h1>Plan page</h1>
+      <div className="card text-center">
+            <div className="card-body">
+                <img src={plan.planImage} alt={plan.title} />
+                <h5 className="card-title">{plan.title}</h5>
+                <p className="card-text">{plan.description}</p>
+                {/* <a href="#" className="btn btn-danger">Go somewhere</a> */}
 
+                <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Update
+                </button>
+            </div>
+
+            {/* <div className="card-footer text-muted">
+                {plan.pricePerDay} €/day
+            </div> */}
+
+            {/* modal */}
+            </div>
     </div>
   );
 }
