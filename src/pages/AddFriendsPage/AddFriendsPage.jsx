e5acb4f7b374fa95fe981389cb8e2583f0bcd913
JB <josepbardesp@gmail.com>
2022-11-07 15:56:30 +0100

    addFriends

--- src/pages/AddFriendsPage/AddFriendsPage.jsx
+++ src/pages/AddFriendsPage/AddFriendsPage.jsx
@@ -1,9 +1,40 @@
 import "./AddFriendsPage.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import userService from "../../services/user.service.js"
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
+import PersonComponent from "../../components/Person/Person.Component";
+
 
 function AddFriendsPage() {
+
+  const { isLoggedIn, user } = useContext(AuthContext);
+  const [addFriends, setAddFriends] = useState([])
+  const [update, setUpdate] = useState(0)
+
+    const updatePeople = (num) => {
+      setUpdate(num)
+    }
+
+    useEffect(()=>{
+      if (isLoggedIn){
+        userService.getAllUsers(user.username)
+        .then (result =>{
+          setAddFriends(result.data)
+        })
+      }    
+    },[isLoggedIn, update])
+
+
   return (
     <div>
+      <p>{user && user.username}</p>
       <h1>Add Friends page</h1>
+      {addFriends.length === 0 && <p>No more people to add</p>}
+      {addFriends.map(person => {
+                return <PersonComponent person={person} updatePeople={updatePeople} key={person._id}/>
+            })}  
     </div>
   );
 }
