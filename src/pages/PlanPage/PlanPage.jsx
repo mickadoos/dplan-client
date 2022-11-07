cb0609fbf3f4dd490416c04099ed84bb88fe2cb4
Yabel <yabelrr@gmail.com>
2022-11-07 15:23:05 +0100

    Setting up Plan Page

--- src/pages/PlanPage/PlanPage.jsx
+++ src/pages/PlanPage/PlanPage.jsx
@@ -1,9 +1,15 @@
 import "./PlanPage.css";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import planService from "../../services/plan.service";
 
 function PlanPage() {
+
+  const {planId} = useParams();
   return (
     <div>
       <h1>Plan page</h1>
+
     </div>
   );
 }
