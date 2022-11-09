41d9a256a7bac5bade5b1adad04dd87dc9052e62
Eloi Pampliega <eloipampliega@gmail.com>
2022-11-09 13:18:16 +0100

    searchbars + filters + others

--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -2,15 +2,17 @@ import "./Plan.css"
 import { Link } from "react-router-dom";
 
 export default function Plan ({plan}) {
+    console.log("plan: ", plan)
     return(
         <div className="card col-3 m-3">
-            <img src={plan.planImage} className="card-img-top" alt={plan.title} />
+            <img src={plan._id.planImage} className="card-img-top" alt={plan._id.title} />
             <div className="card-body">
-                <h5 className="card-title">{plan.title}</h5>
-                <p className="card-text">{plan.description}</p>
-                <p className="card-text">{plan.date}</p>
-                <p className="card-text">{plan.time}</p>
-                <p className="card-text">{plan.location}</p>
+                <h5 className="card-title">{plan._id.title}</h5>
+                <p className="card-text">{plan._id.description}</p>
+                <p className="card-text">{plan._id.date}</p>
+                <p className="card-text">{plan._id.time}</p>
+                <p className="card-text">{plan._id.location}</p>
+                <p className="card-text">Status: {plan.status}</p>
                 <Link to={"/plans/"+plan._id}>View details</Link>
             </div>
         </div>
--- src/pages/AddFriendsPage/AddFriendsPage.jsx
+++ src/pages/AddFriendsPage/AddFriendsPage.jsx
@@ -6,6 +6,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
 import PersonAddFriends from "../../components/Person/PersonAddFriends";
 
+let friendsSearch;
 
 function AddFriendsPage() {
 
@@ -21,18 +22,33 @@ function AddFriendsPage() {
       if (isLoggedIn){
         userService.getAllUsers(user.username)
         .then (result =>{
-          setAddFriends(result.data)
+          friendsSearch = result.data
+          setAddFriends(friendsSearch)
         })
         .catch((err) => console.log(err))
       }    
     },[isLoggedIn, update])
+
+    console.log("addFriends: ", addFriends)
+
+    const formOnChangeHandle = (e) => {
+      console.log("e: ", e.target.value)
+      let searchFriend = [...friendsSearch]
+      setAddFriends(searchFriend.filter(friend => {
+        return (friend.name.toLowerCase().includes(e.target.value.toLowerCase()) || friend.username.toLowerCase().includes(e.target.value.toLowerCase()))
+      }))
+      console.log("searchFriend: ", searchFriend)
+    }
 
 
   return (
     <div>
       <p>{user && user.username}</p>
       <h1>Add Friends page</h1>
-      {addFriends.length === 0 && <p>No more people to add</p>}
+      <form onChange={formOnChangeHandle}>
+        <input placeholder="Search users"/>
+      </form>
+      {addFriends.length === 0 && <p>No results</p>}
       {addFriends.map(person => {
                 return <PersonAddFriends person={person} updatePeople={updatePeople} key={person._id}/>
             })}  
