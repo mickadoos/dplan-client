3614f7e939e3f7a93d533af5038470bb40a874d5
JB <josepbardesp@gmail.com>
2022-11-10 17:24:12 +0100

    Maquetado navBar & addFriends done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,51 +14,47 @@ function Navbar() {
 
     {isLoggedIn && 
       <>
-        <nav className="navbar navbar-dark bg-dark fixed-top">
-          <div className="container-fluid">
-            <Link to={"/plans"} className="navbar-brand"><img className = "logo" src={logo} alt="logo"></img></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-              <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">{user.username}</h5>
-                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div className="offcanvas-body">
-                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                  <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to={"/" + user.username + "/profile"}>My Profile</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to={"/" + user.usename + "/addFriends"}>Add Friends</Link>
-                  </li>
-                  <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Plan
-                    </Link>
-                    <ul className="dropdown-menu dropdown-menu-dark">
-                      <li><Link className="dropdown-item" to="/plans/newPlan">Create a Plan</Link></li>
-                      <li>
-                        <hr className="dropdown-divider"></hr>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-                <form className="d-flex mt-3" role="search">
-                  <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                  <button className="btn btn-success" type="submit">Search</button>
-                </form>
-                <br></br>
-                <button className="btn btn-danger" type="submit" onClick={logOutUser}>Log out</button>
+        <nav className="navbar navbar-dark navBarBackground fixed-top">
+          <div className="container-fluid"> 
+            <div>
+              <Link className="nav-link active" aria-current="page" to={"/" + user.username + "/profile"}><img className = "profileImage" src={user.profileImage} alt="logo"></img></Link>
+            </div>
+            <div>
+              <Link to={"/plans"} className="navbar-brand"><img className = "logo" src={logo} alt="logo"></img></Link>
+            </div>
+            <div>
+              <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div className="offcanvas-header">
+                  <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                  <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li className="nav-item">
+                      <Link to={"/plans"} className="nav-link">Plans Overview</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to={"/" + user.username + "/profile"}>My Profile</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to={"/" + user.usename + "/addFriends"}>Add Friends</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/plans/newPlan">Create a Plan</Link>
+                    </li>
+                  </ul>
+                  <br></br>
+                  <button className="btn btn-danger" type="submit" onClick={logOutUser}>Log out</button>
 
+                </div>
               </div>
             </div>
           </div>
         </nav>
-      <br></br>
-      <br></br>
-              </>
+      
+      </>
             }
 
             {!isLoggedIn && (
